240f425e34c36d828d70c77bc4b0701c33921c38
Crownbutnotaclown <92828070+Crownbutnotaclown@users.noreply.github.com>
2022-05-15 13:50:31 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,20 @@
+# -*- coding:utf -8 -*-
+#!/usr/bin/python3
+
+import random
+
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input()
+length = input()
+number = int(number)
+length = int(length)
+for n in range(number):
+     password =""
+     for i in range(length):
+          password += random.choice(chars)
+     print(password)
+     print('\n')
+
+
+
+
---
+++ prepods.py
@@ -0,0 +1,14 @@
+# -*- coding:utf -8 -*-
+#!/usr/bin/python3
+import math
+import csv
+
+with open('exams.csv') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    for row in reader:
+        Surname = row['Surname']
+        Name = row['Name']
+        LastName = row['LastName']
+        print("", Surname, Name, LastName)
+
+
